7ec9ea6df54c2afbde21fa21e71b162df12440c6
Mahad Raza Khan <130212940+MahadRazaKhan@users.noreply.github.com>
2023-09-12 15:35:07 +0500

    Add files via upload

---
+++ app02.js
@@ -0,0 +1,106 @@
+// Chapter:07
+var calcNum = 2 + (2*6);
+console.log(calcNum);
+// Ans = 14
+
+var calcNum2 = (2+2)*6;
+console.log(calcNum2);
+// Ans = 24
+
+var calcNum3 = (2+2)*(4+2);
+console.log(calcNum3);
+// Ans = 24
+
+var calcNum4 = ((2+2)*4)+2;
+console.log(calcNum4);
+// Ans = 18
+
+var calcNum5 = (2+2)*(4+10);
+console.log(calcNum5);
+// Ans = 56
+
+var calcNum6 = 2+(2*4)+10
+console.log(calcNum6);
+// Ans = 20
+
+var calcNum7 = (4/2)*4;
+console.log(calcNum7);
+// Ans = 8
+
+// Chapter:08
+
+var num = "2"+"2";
+console.log(num);
+// Ans = 22
+
+var num2 = "Hello"+"Dolly";
+alert(num2);
+console.log(num2);
+// Ans = 22
+
+var num3 = "33"+3;
+alert(num3);
+// Ans = 333
+
+var num4 = "Pakistan"+" "+"Zindabad";
+alert(num4);
+// Ans = Pakistan Zindabad
+
+var num5 = "XYZ"+3;
+console.log(num5);
+// Ans = XYZ3
+
+var str = "Mahad"+" "+"Raza";
+    str2 = str +" "+"Khan";
+console.log(str2);
+// Ans = Mahad Raza Khan
+
+// Chapter:09
+
+let firstName = prompt("Enter Your Name:","Guest");
+console.log(firstName);
+
+let country = prompt("Enter Your Country Name:","China");
+console.log(country);
+
+var yourName = prompt("Enter Your Name:");
+console.log(yourName);
+
+var string = prompt("String","Default Input");
+console.log(string);
+
+// Assign strings to two variables
+var message = ("Please enter your name:");
+var defaultResponse = ("John Doe");
+
+// Use the prompt function to get user input
+var userResponse = prompt(message, defaultResponse);
+
+// Display the user's response
+console.log("User's response: " + userResponse);
+
+
+let messageDefaultResponse = prompt("Message","Default Response");
+alert("User's message:",messageDefaultResponse);
+console.log(alert);
+
+var City = ("Karachi");
+if(City == "Karachi"){
+    console.log("The City of Lights")
+};
+
+if (x == y) {
+    var z = prompt("Enter the value of z:");
+}
+
+var ZipCode = ("10010");
+if(ZipCode === "10010"){
+  alert("Karachi");
+} 
+else{
+  alert("Please write correct city");
+};chal raha he ?
+
+
+
+
